a00d7f9a886cb47aa74d183a34d9f2002a6b01a5
Lincoln-Andrade-Silva <lincolnandradesilva@outlook.com>
2025-05-21 12:55:36 -0300

    chore(common): add UserMessages and ProfileMessages constants

---
+++ src/main/java/com/api/core/domain/common/ProfileMessages.java
@@ -0,0 +1,9 @@
+package com.api.core.domain.common;
+
+public class ProfileMessages {
+    public static final String PROFILE_NOT_FOUND = "Profile not found";
+    public static final String PROFILE_EDITED_SUCCESS = "Profile successfully edited.";
+    public static final String PROFILE_NOT_FOUND_BY_ID = "Profile not found for ID: %s";
+    public static final String PROFILE_LISTED_SUCCESS = "Profiles successfully listed.";
+    public static final String PROFILE_CREATED_SUCCESS = "Profile successfully created.";
+}
---
+++ src/main/java/com/api/core/domain/common/UserMessages.java
@@ -0,0 +1,9 @@
+package com.api.core.domain.common;
+
+public class UserMessages {
+    public static final String USER_NOT_FOUND = "User not found";
+    public static final String USER_EDITED_SUCCESS = "User edited successfully.";
+    public static final String USER_NOT_FOUND_BY_ID = "User not found for ID: %s";
+    public static final String USER_LISTED_SUCCESS = "Users successfully listed.";
+    public static final String USER_CREATED_SUCCESS = "User created successfully.";
+}
